2e57fa16855b63dde74c93f27c13ed864e547c3f
Murat Turhan <drmturhan@hotmail.com>
2018-05-30 09:25:41 +0300

    yeni site gecislerine calisiliyor

--- SSB.Spa/src/environments/environment.prod.ts
+++ SSB.Spa/src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: true,
     hmr       : false,
     uygulamaAdi: 'SORU BANK',
-    apiUrl: 'https://soru.azurewebsites.net/api',
+    apiUrl: 'https://sinavsorubankasi.azurewebsites.net/api',
     bosFotoUrl: 'https://ssbdepo.blob.core.windows.net/images/profil_bos_resim.jpg',
-    whitelistedDomains: ['soru.azurewebsites.net', 'res.cloudinary.com'],
+    whitelistedDomains: ['sinavsorubankasi.azurewebsites.net', 'res.cloudinary.com'],
     gorevKontrolSuresi: 5000,
-    fbRedirectUri: 'http://soru.azurewebsites.net'
+    fbRedirectUri: 'https://sinavsorubankasi.azurewebsites.net'
 };
